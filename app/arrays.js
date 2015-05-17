b1fad7e390eacb162a2e836ca82118295f5b8fdf
jgu <jeffwgu@gmail.com>
2015-05-17 15:35:42 -0600

    completed a bit

--- app/arrays.js
+++ app/arrays.js
@@ -3,51 +3,93 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+      return arr.reduce(function(sum, elem) {
+        return sum + elem;
+      })
     },
 
     remove : function(arr, item) {
-
+      arr2 = [];
+      for (i=0; i < arr.length - 1; i++) {
+        if (item !== arr[i]) {
+          arr2.push(arr[i]);
+        }
+      }
+      return arr2;
     },
 
     removeWithoutCopy : function(arr, item) {
+      var i, len;
 
+      for (i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] === item) {
+          arr.splice(i, 1);
+          i = i - 1;
+          len = len - 1;
+        }
+      }
+
+      return arr;
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr;
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     prepend : function(arr, item) {
-
+      arr.unshift(item);
+      return arr;
     },
 
     curtail : function(arr) {
-
+      arr.shift();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
-
+      return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-
+      arr.splice(index, 0, item);
+      return arr;
     },
 
     count : function(arr, item) {
-
+      var count = 0;
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          count++;
+        }
+      }
+      return count;
     },
 
     duplicates : function(arr) {
+      var seen = {};
+      var dupes = [];
 
+      for (var i = 0; i < arr.length; i++) {
+        seen[arr[i]] = seen[arr[i]] + 1 || 1;
+      }
+
+      for (var jeffrey in seen) {
+        if (seen[jeffrey] && seen[jeffrey] > 1) {
+          dupes.push(jeffrey);
+        }
+      }
+
+      return dupes;
     },
 
     square : function(arr) {
--- app/flowControl.js
+++ app/flowControl.js
@@ -3,14 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+      var string = "";
+      if (typeof num !== "number") {
+        return false;
+      }
+
+      if (num % 3 !== 0 && num % 5 !== 0) {
+        return num;
+      }
+
+      if (num % 3 === 0) {
+        string += "fizz";
+      }
+
+      if (num % 5 === 0) {
+        string += "buzz";
+      }
+      return string;
     }
   };
 });
